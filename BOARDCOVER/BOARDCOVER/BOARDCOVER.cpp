f1e64122b8354a500688d2fd8fcf028f05a9b7e9
JournKim <wndnjs1769@naver.com>
2016-08-11 17:51:57 +0900

    Add files via upload
    BOARDCOVER cont

---
+++ BOARDCOVER/BOARDCOVER/BOARDCOVER.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+¡¸ : coverType[0]
+¤¡ : coverType[1]
+¤¤ : coverType[2]
+¡¹ : coverType[3]
+*/
+const int coverType[4][3][2] = {
+	{{0,0},{1,0},{0,1}},
+	{{0,0},{0,1},{1,1}},
+	{{0,0},{1,0},{1,1}},
+	{{0,0},{1,0},{1,-1}}
+};
+
+void boardcover()
+{
+	bool board[21][21] = { false, };
+	int h, w,count=0;
+	cin >> h >> w;
+	vector<vector<char>> v(h,vector<char>(w));
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++) {
+			scanf("%c", &v[i][j]);
+			if (v[i][j] == '.') ++count;
+
+		}
+	}
+	if (count % 3 != 0) {
+		printf("0\n");
+		return;
+	}
+
+	
+
+
+}
+int main()
+{
+	int cases;
+	cin >> cases;
+	while (cases--)
+	{
+		boardcover();
+	}
+}
